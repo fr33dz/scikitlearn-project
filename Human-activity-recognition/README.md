ded91781226afebc889d3168d7d03ef3a83a8639
fr33dz <yacine.bouslahi@gmail.com>
2017-12-22 09:57:10 +0100

    add readme file

---
+++ Human-activity-recognition/README.md
@@ -0,0 +1,55 @@
+﻿ Utilisation des algorithmes de classifications pour reconnaître l'activité humaine
+  
+
+ **1. les activités :**
+1 MARCHER
+2 MARCHER À L'ÉTAGE
+3 MARCHER EN DESCENTE
+4 ASSIS
+5 DEBOUT
+6 POSE(allongé)
+
+ **2. les algorithme de classifications**
+   K-NN
+   Tree
+   SVM
+   MLP
+   NB
+  10-fold cross validation comme méthode de validation
+  
+ **3. Resultats**
+10-NN ALGO 
+cv=10 
+Precision=0.9103002441782835  
+Temps=36.39 s           
+
+--------------------------
+Tree ALGO 
+cv=10 
+Precision=0.8677449277262987  
+Temps=40.767 s           
+
+--------------------------
+SVM ALGO 
+cv=10 
+Precision=0.9193966024924233  
+Temps=75.563 s           
+
+--------------------------
+MLP ALGO 
+cv=10 
+Precision=0.9386072998352629  
+Temps=44.129 s           
+
+--------------------------
+Gaussian Naive Bayes ALGO 
+cv=10 
+Precision=0.6978641751867267 
+Temps=1.129 s           
+
+--------------------------
+
+![graphe de comparaison entre les algorithmes selon la précision](https://raw.githubusercontent.com/fr33dz/scikitlearn-project/Human-activity-recognition/algo.png)
+
+
+
--- Human-activity-recognition/test_tp2.1.py
+++ Human-activity-recognition/test_tp2.1.py
@@ -1,35 +1,42 @@
+import time, os, sys
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import cross_val_score
-import time
+from sklearn.metrics import accuracy_score
 
+
+path = os.getcwd()
 # Afectation de X (Variables) et y (Sorties)
-X = np.loadtxt('E:/TP MATLAB/tp2/tp2 python/X_train.txt')
-y = np.loadtxt('E:/TP MATLAB/tp2/tp2 python/y_train.txt')
+X = np.loadtxt(path+'/data/X_train.txt')
+y = np.loadtxt(path+'/data/y_train.txt')
 
 ##Affichage 
 def report(algo, cv, pecision, temps):
     print("{} ALGO \ncv={} \nPrecision={}  \nTemps={} s \
-          ".format(algo, cv , round(pecision, 3), round(temps, 3)))
+          ".format(algo, cv , pecision , round(temps, 3)))
     print("--------------------------")
+
+
+
 
 K=10
 CV = 10
-tab_scores = {}
+
 
 start_time = time.time()
 #Application de l'algo KNN avec K=4 et la validation croisée 10 iterations
 knn = KNeighborsClassifier(n_neighbors=K)
-scores = cross_val_score(knn, X, y, cv=CV, scoring='recall')
+scores = cross_val_score(knn, X, y, cv=CV, scoring='accuracy')
 plt.plot(scores, label="10-NN")
 #Affichage 
 report("10-NN",CV ,scores.mean(), (time.time() - start_time))
+#sys.exit(0)
 
 from sklearn import tree
 start_time = time.time()
 clf_tree = tree.DecisionTreeClassifier()
-scores = cross_val_score(clf_tree, X, y, cv=CV, scoring='recall')
+scores = cross_val_score(clf_tree, X, y, cv=CV, scoring='accuracy')
 plt.plot(scores, label="TREE")
 #Affichage 
 report("Tree",CV ,scores.mean(), (time.time() - start_time))
@@ -37,7 +44,7 @@ report("Tree",CV ,scores.mean(), (time.time() - start_time))
 from sklearn import svm
 start_time = time.time()
 clf_svm = svm.SVC()
-scores = cross_val_score(clf_svm, X, y, cv=CV, scoring='recall')
+scores = cross_val_score(clf_svm, X, y, cv=CV, scoring='accuracy')
 plt.plot(scores, label="SVM")
 ##Affichage 
 report("SVM",CV ,scores.mean(), (time.time() - start_time))
@@ -45,7 +52,7 @@ report("SVM",CV ,scores.mean(), (time.time() - start_time))
 from sklearn.neural_network import MLPClassifier
 start_time = time.time()
 clf_MLP = MLPClassifier()
-scores = cross_val_score(clf_MLP, X, y, cv=CV, scoring='recall')
+scores = cross_val_score(clf_MLP, X, y, cv=CV, scoring='accuracy')
 plt.plot(scores, label="MLP")
 ##Affichage 
 report("MLP",CV ,scores.mean(), (time.time() - start_time))
@@ -53,7 +60,7 @@ report("MLP",CV ,scores.mean(), (time.time() - start_time))
 from sklearn.naive_bayes import GaussianNB
 start_time = time.time()
 clf_gnb = GaussianNB()
-scores = cross_val_score(clf_gnb, X, y, cv=CV, scoring='recall')
+scores = cross_val_score(clf_gnb, X, y, cv=CV, scoring='accuracy')
 plt.plot(scores, label="GHB")
 ##Affichage 
 report("Gaussian Naive Bayes",CV ,scores.mean(), (time.time() - start_time))
